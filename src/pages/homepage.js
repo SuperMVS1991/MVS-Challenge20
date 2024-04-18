77977791254cc7dcbd513645ff53549cdade1744
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-04-18 13:44:20 -0400

    added navigation to about page

--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -16,17 +16,6 @@ function Homepage() {
         DESKTOP TECHNICIAN
       </p>
       <div className="navbar navbar-expand-lg card">
-        <p>
-          I am a web developer and desktop technician. I am a graduate of the
-          University of Connecticut with a degree in Web Development. I have
-          experience with HTML, CSS, JavaScript, and React. I am also graduate
-          from Porter & Chester Institute of Technology in Hardware, Networking,
-          and Software Troubleshooting. I have experience with Windows, MacOS,
-          and Linux. I am also familiar with Active Directory, Group Policy, and
-          PowerShell. I have experience with virtualization, networking, and
-          cybersecurity. I am a quick learner and I am always looking to learn
-          new technologies.
-        </p>
       </div>
       <div className="navbar navbar-expand-lg card container">
           <img className="col-8" src={Passcode} alt="" />
