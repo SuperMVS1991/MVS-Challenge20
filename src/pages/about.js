e13fd76bcc8b5faa0909596638a39980303c6d26
SuperMVS1991 <150755455+SuperMVS1991@users.noreply.github.com>
2024-04-18 13:45:27 -0400

    used pritier format

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,22 @@
 import Navigation from "../components/navigation";
 
 function About() {
- 
-    return (
+  return (
     <div>
-        <Navigation />
+      <Navigation />
       <div className="navbar navbar-expand-lg card">
-      <h1>About</h1>
-      <p>
-        I am a web developer and desktop technician. I am a graduate of the
-        University of Connecticut with a degree in Web Development. I have
-        experience with HTML, CSS, JavaScript, and React. I am also graduate
-        from Porter & Chester Institute of Technology in Hardware, Networking,
-        and Software Troubleshooting. I have experience with Windows, MacOS, and
-        Linux. I am also familiar with Active Directory, Group Policy, and
-        PowerShell. I have experience with virtualization, networking, and
-        cybersecurity. I am a quick learner and I am always looking to learn new
-        technologies.
-      </p>
+        <h1>About</h1>
+        <p>
+          I am a web developer and desktop technician. I am a graduate of the
+          University of Connecticut with a degree in Web Development. I have
+          experience with HTML, CSS, JavaScript, and React. I am also graduate
+          from Porter & Chester Institute of Technology in Hardware, Networking,
+          and Software Troubleshooting. I have experience with Windows, MacOS,
+          and Linux. I am also familiar with Active Directory, Group Policy, and
+          PowerShell. I have experience with virtualization, networking, and
+          cybersecurity. I am a quick learner and I am always looking to learn
+          new technologies.
+        </p>
       </div>
     </div>
   );
--- src/pages/homepage.js
+++ src/pages/homepage.js
@@ -15,59 +15,58 @@ function Homepage() {
         WELCOME MY NAME IS MICHAEL SLOTNICK AND I AM A WEB DEVELOPER AND A
         DESKTOP TECHNICIAN
       </p>
-      <div className="navbar navbar-expand-lg card">
+      <div className="navbar navbar-expand-lg card"></div>
+      <div className="navbar navbar-expand-lg card container">
+        <img className="col-8" src={Passcode} alt="" />
+        <Link to="https://github.com/SuperMVS1991/MVS-Challenge3">
+          <button type="button" className="btn btn-primary">
+            View Code
+          </button>
+        </Link>
+        <Link to="https://supermvs1991.github.io/MVS-Challenge3/">
+          <button type="button" className="btn btn-secondary">
+            See More
+          </button>
+        </Link>
       </div>
       <div className="navbar navbar-expand-lg card container">
-          <img className="col-8" src={Passcode} alt="" />
-          <Link to="https://github.com/SuperMVS1991/MVS-Challenge3">
-            <button type="button" className="btn btn-primary">
-              View Code
-            </button>
-          </Link>
-          <Link to="https://supermvs1991.github.io/MVS-Challenge3/">
-            <button type="button" className="btn btn-secondary">
-              See More
-            </button>
-          </Link>
+        <img className="col-8" src={techBlog} alt="" />
+        <Link to="https://github.com/SuperMVS1991/Challenge14">
+          <button type="button" className="btn btn-primary">
+            View Code
+          </button>
+        </Link>
+        <Link to="https://supermvs1991.github.io/Challenge14/">
+          <button type="button" className="btn btn-secondary">
+            See More
+          </button>
+        </Link>
       </div>
       <div className="navbar navbar-expand-lg card container">
-          <img className="col-8" src={techBlog} alt="" />
-          <Link to="https://github.com/SuperMVS1991/Challenge14">
-            <button type="button" className="btn btn-primary">
-              View Code
-            </button>
-          </Link>
-          <Link to="https://supermvs1991.github.io/Challenge14/">
-            <button type="button" className="btn btn-secondary">
-              See More
-            </button>
-          </Link>
+        <img className="col-8" src={MWF} alt="" />
+        <Link to="https://github.com/SuperMVS1991/Movies-with-Friends">
+          <button type="button" className="btn btn-primary">
+            View Code
+          </button>
+        </Link>
+        <Link to="https://movies-friends-85c9074d5005.herokuapp.com/">
+          <button type="button" className="btn btn-secondary">
+            See More
+          </button>
+        </Link>
       </div>
       <div className="navbar navbar-expand-lg card container">
-          <img className="col-8" src={MWF} alt="" />
-          <Link to="https://github.com/SuperMVS1991/Movies-with-Friends">
-            <button type="button" className="btn btn-primary">
-              View Code
-            </button>
-          </Link>
-          <Link to="https://movies-friends-85c9074d5005.herokuapp.com/">
-            <button type="button" className="btn btn-secondary">
-              See More
-            </button>
-          </Link>
-      </div>
-      <div className="navbar navbar-expand-lg card container">
-          <img className="col-8" src={marvelMind} alt="" />
-          <Link to="https://supermvs1991.github.io/Project1/">
-            <button type="button" className="btn btn-primary">
-              View Code
-            </button>
-          </Link>
-          <Link to="https://github.com/SuperMVS1991/Project1">
-            <button type="button" className="btn btn-secondary">
-              See More
-            </button>
-          </Link>
+        <img className="col-8" src={marvelMind} alt="" />
+        <Link to="https://supermvs1991.github.io/Project1/">
+          <button type="button" className="btn btn-primary">
+            View Code
+          </button>
+        </Link>
+        <Link to="https://github.com/SuperMVS1991/Project1">
+          <button type="button" className="btn btn-secondary">
+            See More
+          </button>
+        </Link>
       </div>
     </div>
   );
